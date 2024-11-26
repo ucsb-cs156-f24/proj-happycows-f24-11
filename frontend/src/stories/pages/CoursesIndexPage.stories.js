0b6836211b800dc03e4eddfef5210d98c85d7ecb
Aden <adenjo@ucsb.edu>
2024-11-25 18:53:54 -0800

    removed instructor

--- frontend/src/stories/pages/CoursesIndexPage.stories.js
+++ frontend/src/stories/pages/CoursesIndexPage.stories.js
@@ -32,26 +32,6 @@ Empty.parameters = {
     ]
 }
 
-export const ThreeItemsInstructorUser = Template.bind({});
-
-ThreeItemsInstructorUser.parameters = {
-    msw: [
-        rest.get('/api/currentUser', (_req, res, ctx) => {
-            return res( ctx.json(apiCurrentUserFixtures.instructorUser));
-        }),
-        rest.get('/api/systemInfo', (_req, res, ctx) => {
-            return res(ctx.json(systemInfoFixtures.showingNeither));
-        }),
-        rest.get('/api/courses/all', (_req, res, ctx) => {
-            return res(ctx.json(coursesFixtures.threeCourses));
-        }),
-        rest.delete('/api/courses', (req, res, ctx) => {
-            window.alert("DELETE: " + JSON.stringify(req.url));
-            return res(ctx.status(200),ctx.json({}));
-        }),
-    ],
-}
-
 export const ThreeItemsAdminUser = Template.bind({});
 
 ThreeItemsAdminUser.parameters = {
